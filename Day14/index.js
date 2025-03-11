5e07cf7c7cc7179f0d335a086a7553a2596294a5
tajbid-panthom <tajbidhossain903@gmail.com>
2025-03-11 23:59:38 +0600

    Day14 Done

---
+++ Day14/index.js
@@ -0,0 +1,11 @@
+let name = "Tajbid Hossain";
+
+console.log(
+  name,
+  name.length,
+  "First Char is: ",
+  name[0],
+  " and the last char is: ",
+  name[name.length - 1],
+  "apnacollege" + 123 //print apnacollege123
+);
