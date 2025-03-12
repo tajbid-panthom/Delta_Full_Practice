3b3a355e735d7220848341adc1e46ed92dd48b35
tajbid-panthom <tajbidhossain903@gmail.com>
2025-03-12 23:10:37 +0600

    Day16 Done

---
+++ Day16/Qs.js
@@ -0,0 +1,15 @@
+// qs1
+
+let arr = [12, 2, 4, 5];
+let n = 3;
+
+console.log(arr.slice(0, n));
+
+console.log(arr.slice(-n));
+
+"Hi" ? console.log("Not blank") : console.log("blank");
+
+let str = "TAjDid";
+str[3] === str[3].toLowerCase() ? console.log("Low") : console.log("High");
+
+arr.includes(4) ? console.log("It is") : console.log("It is not");
---
+++ Day16/index.js
@@ -0,0 +1,7 @@
+let coding = "I love coding\n";
+console.log("I love you \n".repeat(5), coding.replaceAll("o", "x"));
+
+let msg = "help!";
+console.log(msg.trim("!").toUpperCase());
+let college = "ApnaCollege";
+console.log(college.slice(4).replace("l", "t"));
