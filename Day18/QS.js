544883d4d3e517b8d904d627fd2c925d1bb82499
tajbid-panthom <tajbidhossain903@gmail.com>
2025-03-15 05:08:22 +0600

    Day18 Done

---
+++ Day18/QS.js
@@ -0,0 +1,20 @@
+let _random = Math.floor(Math.random() * 6 + 1);
+console.log(_random);
+
+const car = {
+  name: "Toyota",
+  model: "Camry",
+  year: 2020,
+};
+console.log(car);
+
+const person = {
+  name: "John",
+  age: 30,
+  gender: "male",
+  city: "dhaka",
+};
+person.city = "New Work";
+person.country = "USA";
+
+console.log(person);
---
+++ Day18/index.js
@@ -0,0 +1,5 @@
+let one_to_hundred = Math.floor(Math.random() * 100 + 1);
+let one_to_five = Math.floor(Math.random() * 5 + 1);
+
+console.log(one_to_hundred);
+console.log(one_to_five);
